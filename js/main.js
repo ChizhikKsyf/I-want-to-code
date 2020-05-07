83fd602f430d5caecf9fe62b1d9bbb3cd8fa4a5b
Chizhik Kseniya <31064426+ChizhikKsyf@users.noreply.github.com>
2020-05-07 20:29:14 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+	$('.offer-content a').click(function () {
+		$('.triangles').css('width','300px');
+	});
+	$('.logo').click(function () {
+		$('.offer-content h1').css('font-size','10px');
+	});
+
+});
